ccae4cfd635379ead8a0371a88de8eea83b7257b
Max Arbakov <fefaodessa@gmail.com>
2023-09-21 23:09:38 +0300

    start prdjeckt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ToastContainer } from "react-toastify";
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
+const Layout = lazy(() => import("./layout/Layout"));
+const Home = lazy(() => import("./pages/Home"));
+const PageNotFound = lazy(() => import("./pages/pageNotFound/PageNotFound"));
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Suspense fallback={<h1>Loading....</h1>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
+      <ToastContainer autoClose={2000} />
+    </>
   );
-}
-
-export default App;
+};
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import "./Footer.css";
+import "../../ui/container.css";
+
+export const Footer = () => {
+  return (
+    <footer className="footer__section">
+      <div className="container"></div>
+    </footer>
+  );
+};
